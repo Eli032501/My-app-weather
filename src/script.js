d3500368654a1211f3b7108d5dbf7534cdebfda7
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2023-12-11 17:54:45 +0000

    function url + apikey+ place

--- src/script.js
+++ src/script.js
@@ -1,11 +1,16 @@
-function searchPlaceSubmit(event) {
+function searchPlace(place) {
+  let apiKey = "ba3t92f6af6b8204143cbo1c5a032ba3";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${place}&key=${apiKey}&units=metric`;
+  console.log(url);
+}
+
+function displayPlaceSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input_search");
-  let valueSearchInput = searchInput.value;
-
   let placeDisplay = document.querySelector("h1");
-  placeDisplay.innerHTML = valueSearchInput.trim().toLowerCase();
+  placeDisplay.innerHTML = searchInput.value;
+  searchPlace(searchInput.value);
 }
 
 let searchForm = document.querySelector("#form_search");
-searchForm.addEventListener("submit", searchPlaceSubmit);
+searchForm.addEventListener("submit", displayPlaceSubmit);
