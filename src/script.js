7a2824901244e50c2480f30e243d8d536eca112d
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2023-12-11 17:43:54 +0000

    add axios + search input working

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-alert("hellooo");
+function searchPlaceSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#input_search");
+  let valueSearchInput = searchInput.value;
+
+  let placeDisplay = document.querySelector("h1");
+  placeDisplay.innerHTML = valueSearchInput.trim().toLowerCase();
+}
+
+let searchForm = document.querySelector("#form_search");
+searchForm.addEventListener("submit", searchPlaceSubmit);
